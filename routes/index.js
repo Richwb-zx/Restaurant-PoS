77b04bb298f42e1d00e980bcc7fdf00ab773bdcb
Richwb <57405742+Richwb@users.noreply.github.com>
2021-02-25 23:21:29 -0500

    created landing login and logout routes

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) =>{
+   
+});
+
+router.post('/login', (req, res) =>{
+   
+});
+
+router.post('/logout', (req, res) =>{
+ 
+});
+
+module.exports = router;
