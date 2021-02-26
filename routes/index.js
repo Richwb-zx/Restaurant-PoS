8f767dfe59a21e2a924380895e5230d2f29a8538
Richwb <57405742+Richwb@users.noreply.github.com>
2021-02-26 15:09:15 -0500

    added registration route (#4)
    * disabled unused packages
    
    * set port to run on 3k if not enviroment
    
    * created landing login and logout routes
    
    * added registration route

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) =>{
+
+});
+
+router.post('/login', (req, res) =>{
+   
+});
+
+router.post('/logout', (req, res) =>{
+ 
+});
+
+router.post('/register', (req, res) =>{
+ 
+});
+
+module.exports = router;
