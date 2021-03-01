6acf6562cb553f84a9b20130929c1d773531c366
Richwb <57405742+Richwb@users.noreply.github.com>
2021-02-28 21:32:57 -0500

    initialized authentication for login

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const authentication = require('../controller/authentication.js');
 
 router.get('/', (req, res) =>{
 
 });
 
 router.post('/login', (req, res) =>{
-   
+    const authenticate = new authentication(req.query.userName, req.query.password);
 });
 
 router.post('/logout', (req, res) =>{
