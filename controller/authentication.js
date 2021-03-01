dc72ffc4352fb998a2f9008699e362393bb4899b
Richwb <57405742+Richwb@users.noreply.github.com>
2021-03-01 00:50:24 -0500

    implimented bcryptjs

--- controller/authentication.js
+++ controller/authentication.js
@@ -1,4 +1,5 @@
 const user = require('../services/user.js');
+const bcryptjs = require('bcryptjs');
 
 const Autentication = class Authentication{
     constructor(account, password){
@@ -9,6 +10,14 @@ const Autentication = class Authentication{
     user(){
         const userService = new user(this.account);
         userService.getUser();
+    }
+
+    bcryptHash(){
+        return bcrypt.hashSync(this.password, bcrypt.genSaltSync(13));
+    }
+
+    bcryptCompare(pwHash){
+        return bcrypt.compareSync(this.password, hash); // true
     }
 
 }
