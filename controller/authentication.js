6710d472ecee9a1b29f47e493dc7a7fee47e3981
Richwb <57405742+Richwb@users.noreply.github.com>
2021-02-28 21:14:11 -0500

    created Authentication class, will handle all auth

---
+++ controller/authentication.js
@@ -0,0 +1,9 @@
+const Autentication = class Authentication{
+    constructor(name, password){
+        this.Name = name;
+        this.password = password;
+    }
+
+}
+
+module.exports = Autentication;
