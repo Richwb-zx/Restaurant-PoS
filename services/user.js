a44a18d110693b367e8a66cd0443464dff4765ca
Richwb <57405742+Richwb@users.noreply.github.com>
2021-02-28 23:29:24 -0500

    method that ueries user table for account

--- services/user.js
+++ services/user.js
@@ -1,6 +1,28 @@
-const User = class User{
+const userModel = require('../models/users.js');
 
-    get authenticate(){
-        
+const User = class User{
+    constructor(username, password){
+        this.userName = username;
+        this.password = password;
+    }
+
+    async authenticate(){
+        return await userModel
+            .findAll({
+                raw: true,
+                where: {
+                    user_name: this.userName, 
+                    password: this.password, 
+                    active: 0
+                }
+            })
+            .then(result => {
+                return (result.length > 0 ? true : false);
+            })
+            .catch(error => {
+                console.log(error);
+            });  
     }
 }
+
+module.exports = User;
