6c69ca162d11982b4b4fce89f75abf21cfc8104b
Richwb <57405742+Richwb@users.noreply.github.com>
2021-03-02 21:01:26 -0500

    called user auth controller function

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const authentication = require('../controller/authentication.js');
+const bcryptjs = require('bcryptjs');
 
 router.get('/', (req, res) =>{
 
 });
 
-router.post('/login', (req, res) =>{
+router.post('/login', async (req, res) =>{
+    
     const authenticate = new authentication(req.query.userName, req.query.password);
+    await authenticate.user();
 });
 
 router.post('/logout', (req, res) =>{
