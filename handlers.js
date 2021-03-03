cd1562dbe50d74edc4a8ced7f72422128cb4cedd
Richwb <57405742+Richwb@users.noreply.github.com>
2021-03-03 02:37:16 -0500

    verifies session and redirects appropriately

--- handlers.js
+++ handlers.js
@@ -1,14 +1,43 @@
+const jwt = require('jsonwebtoken');
+
 const checkSession = (req, res, next) => {
-    console.log(req.cookies.token);
+    
     const token = req.cookies.token;
+    const path = req.path;
+    console.log(token,process.env.node_sess_secret);
 
-    if(!token && req.path !== '/login' ){
+    if(!token && path == '/login'){
+        next();
+    }else if(!token && path !== '/login'){
         res.redirect('/login');
-    }else if(token && req.path !== '/login'){
-        res.redirect('/');
+    }else{
+        response = jwt.verify(token, process.env.node_sess_secret, (error, decoded) => {       
+            if(error){
+                switch(error['name']){
+                    case 'TokenExpiredError':
+                        switch(path){
+                            case '/login':
+                                next();
+                                break;
+                            default:
+                                res.redirect('/login');
+                        }
+                        break;
+                    default:
+                        console.log(error);
+                }
+            }else if(decoded){
+                switch(path){
+                    case '/login':
+                        res.redirect('/test');    
+                        break;
+                    default:
+                        next();
+                }
+            }
+        });
+        
     }
-
-    next();
 }
 
 module.exports = checkSession;
