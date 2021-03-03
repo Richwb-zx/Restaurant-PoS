38d0b59ddab8f77fe39168cfba96a52e3c8f9b34
Richwb <57405742+Richwb@users.noreply.github.com>
2021-03-03 01:04:42 -0500

    enabled cookie parser

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 // const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -10,9 +10,10 @@ const app = express();
 // app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
 
 module.exports = app;
