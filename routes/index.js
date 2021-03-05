0fec1704bb0b81ed1084ab65c458149d7d625559
Richwb <57405742+Richwb@users.noreply.github.com>
2021-03-04 20:53:52 -0500

    moved logic handlers to controller for seperatio of duties

--- routes/index.js
+++ routes/index.js
@@ -29,23 +29,8 @@ router.post('/logout', (req, res) =>{
 
 router.post('/register', async(req, res) =>{
     const authenticate = new authentication(req.query.userName, req.query.password);
-    const registerResult = await authenticate.register();
-
-    let returnMsg = 'An error occured';
-    let returnStatus = false;
-    let resStatus = 500;
-
-    if(registerResult === true){
-        returnMsg = 'Registration Successful';
-        returnStatus = true;
-        resStatus = 200;
-    }else if(typeof(registerResult) === 'string'){
-        returnMsg = registerResult;
-        returnStatus = false;
-        resStatus = 200;
-    }
-
-    res.status(resStatus).json({msg: returnMsg, status: returnStatus});
+    const registerResult = await authenticate.register();    
+    res.status(registerResult[1].httpStatus).json(registerResult[0]);
 });
 
 module.exports = router;
