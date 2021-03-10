84cd998df106adfd3f6fb7293442fc737ead6efd
Richwb <57405742+Richwb@users.noreply.github.com>
2021-03-09 23:01:07 -0500

    simplified user auth controller via user service

--- controller/authentication.js
+++ controller/authentication.js
@@ -8,37 +8,29 @@ const Autentication = class Authentication{
     }
 
     async user(){
-        const loginResult = [];
         let bcryptToken = false;
 
         const userService = new user(this.account);
         const userResult = await userService.getUser();
-        const resultId = userResult.response.id;
-        
-        if(userResult.success === false){
-            return loginResult.push(userResult, {httpStatus: 500});
-        }else if(resultId !== undefined){
+       
+        if(userResult.error === true){
+            return [{response: 'An Unexpected error has occured, Admin have been notified', success: false},{httpStatus: 500}];
+        }else if(userResult.success === true){
             bcryptToken = this.bcryptCompare(userResult.response.password);
         }
-        
-        if(resultId === undefined || bcryptToken === false){
-            loginResult.push({response:'Incorrect Username or Password.', success: false},{httpStatus: 200});
-        }else if(userResult.success === true && bcryptToken !== false){
+
+        if(bcryptToken === false || userResult.success === false){
+            return [{response:'Incorrect Username or Password.', success: false},{httpStatus: 200}];
+        }else if(bcryptToken === true){
             const token = userService.setSession();
-            loginResult.push({response: 'Login Successful', success: true},{token: token,httpStatus: 200});
-        }else{
-            // TODO error logging 
-            loginResult.push({response: 'An Unexpected error has occured, Admin have been notified', success: false},{httpsStatus: 500});
+            return [{response: 'Login Successful', success: true},{token: token,httpStatus: 200}];
         }
-
-        return loginResult;        
-
     }
 
     async register(){
         const userService = new user(this.account);
         const pwHash = this.bcryptHash();
-        return userService.createUser(pwHash);
+        return await userService.createUser(pwHash);
     }
 
     logout(){
