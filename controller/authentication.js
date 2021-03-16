ee486fa98c768d1db98478803afb6717347b2f1d
Richwb <57405742+Richwb@users.noreply.github.com>
2021-03-15 22:11:58 -0400

    added ip to constructor, called user service in constructor, change to user

--- controller/authentication.js
+++ controller/authentication.js
@@ -2,16 +2,16 @@ const user = require('../services/user.js');
 const bcryptjs = require('bcryptjs');
 
 const Autentication = class Authentication{
-    constructor(account, password){
+    constructor(account, password, ip){
         this.account = account;
         this.password = password;
+        this.ip = ip;
+        this.userService = new user(this.account, this.ip);
     }
 
     async user(){
         let bcryptToken = false;
-
-        const userService = new user(this.account);
-        const userResult = await userService.getUser();
+        const userResult = await this.userService.getUser();
        
         if(userResult.error === true){
             return [{response: 'An Unexpected error has occured, Admin have been notified', success: false},{httpStatus: 500}];
@@ -19,18 +19,30 @@ const Autentication = class Authentication{
             bcryptToken = this.bcryptCompare(userResult.response.password);
         }
 
+        if(bcryptToken === false && userResult.response.locked === 0){
+            const inValidCheck = await this.userService.invalidLogin(userResult);
+
+            if(inValidCheck.sucess === true){
+                return [{response:'Account has been locked.', success: false},{httpStatus: 200}];
+            }  
+
+        }else if(userResult.response.locked === 1){
+            return [{response:'Account is locked.', success: false},{httpStatus: 200}];
+        }else if(userResult.response.active === 1){
+            return [{response:'Account is inactive.', success: false},{httpStatus: 200}];
+        }
+
         if(bcryptToken === false || userResult.success === false){
             return [{response:'Incorrect Username or Password.', success: false},{httpStatus: 200}];
         }else if(bcryptToken === true){
-            const token = userService.setSession();
+            const token = this.userService.setSession();
             return [{response: 'Login Successful', success: true},{token: token,httpStatus: 200}];
         }
     }
 
     async register(){
-        const userService = new user(this.account);
         const pwHash = this.bcryptHash();
-        const registerResponse = await userService.createUser(pwHash);
+        const registerResponse = await this.userService.createUser(pwHash);
 
         if(registerResponse[1].error === true){
             registerResponse[1].httpStatus = 500;
@@ -48,8 +60,7 @@ const Autentication = class Authentication{
     }
 
     logout(){
-        const userService = new user(this.account);
-        return userService.setSession(true);
+        return this.userService.setSession(true);
     }
 
     bcryptHash(){
