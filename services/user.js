ba0b50476f57f585790a3032a0ece20300c30ec5
Richwb <57405742+Richwb@users.noreply.github.com>
2021-03-15 22:10:02 -0400

    added function to process invalid logins, increases the failed count and then disables after 3 tries

--- services/user.js
+++ services/user.js
@@ -1,25 +1,20 @@
 const userModel = require('../models/users.js');
+const {AuthTimeoutModel, knex} = require('../models/authorization_timeout.js');
 const jwt = require('jsonwebtoken');
 
 const User = class User{
-    constructor(username, pwHash){
+    constructor(username, ip){
         this.userName = username;
+        this.ip = ip;
     }
 
-    async getUser(checkActive = 1, checkLocked = 0){
+    async getUser(){
              
         const userQuery = userModel.query().select().where('user_name', this.userName)
-
-        if(checkActive !== false){
-            userQuery.where('active', checkActive);
-        }
-
-        if(checkLocked !== false){
-            userQuery.where('locked', checkLocked);
-        }
         
         return await userQuery
             .then(result => {
+                console.log(result);
                 const success = (result.length > 0 ? true : false);
                 return {response: result[0], success: success, error: false};
             })
@@ -55,6 +50,40 @@ const User = class User{
 
                                     return response;
                                 });        
+        
+    }
+
+    async invalidLogin(userResult){
+        console.log('hi');
+        const userId = userResult.response.id;      
+        //TODO universal date function
+        const date = Math.round(Date.now() / 1000);
+        
+
+        knex.raw('INSERT INTO `authorization_timeout` (`user_id`, `number_attempts`, `ip_address`,`last_attempt`, `created_on`) VALUES (?,?,?,?,?) ON DUPLICATE KEY UPDATE `number_attempts` = `number_attempts` + 1, `last_attempt` = ?', [userId, 1, this.ip,date,date,date])
+        .then(() => {
+            return knex.raw('UPDATE users u INNER JOIN authorization_timeout at ON (at.user_id = u.id) SET u.locked=1, u.locked_on=? WHERE at.number_attempts = 3 AND at.user_id=?',[date,userId])
+            .then(updateResult => {
+                let response = '';
+                let success = false;
+                if(updateResult[0].changedRows === 1){
+                   response = 'Account has been locked, please wait 5 minutes';
+                   success = true;
+                }
+
+                return {response: response, success: success, error: false};
+
+            }).catch(error2 => {
+                //TODO error logging
+                console.log(error2);
+            });
+        })
+        .catch(error => {
+            //TODO error logging
+            console.log(error);
+        });
+
+
         
     }
 }
