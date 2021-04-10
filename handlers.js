494941a2a3b49e7738f1c0ea41d206aa1433c35e
Richwb <57405742+Richwb@users.noreply.github.com>
2021-04-09 22:00:44 -0400

    had to rework the jwt verify to work off of a promise. in order to allow the redis client calls to also work off of a promise. Proceeded to add the promises which check to make sure that the token being used is not on the blacklist, if it is return response with 401.

--- handlers.js
+++ handlers.js
@@ -1,8 +1,13 @@
 const jwt = require('jsonwebtoken');
 const redis = require('redis');
 const client = redis.createClient();
+const { promisify } = require('util');
+const jwtverify = promisify(jwt.verify).bind(jwt);
+const clientKeys = promisify(client.keys).bind(client);
+const clientlrange = promisify(client.lrange).bind(client);
 
-const checkSession = (req, res, next) => {
+const checkSession = async (req, res, next) => {
+
     const token = req.cookies.token;
 
     const path = req.path;
@@ -14,46 +19,75 @@ const checkSession = (req, res, next) => {
     }else if(token === undefined && path !== '/login'){
         res.redirect('/login');
     }else{
-        response = jwt.verify(token, process.env.node_sess_secret, (error, decoded) => {       
-            if(error){
-                switch(error['name']){
-                    case 'TokenExpiredError':
-                        switch(path){
-                            case '/login':
-                                res.clearCookie('token');
-                                next();
-                                break;
-                            default:
-                                res.redirect('/login');
-                        }
-                        break;
-                    default:
-                        // TODO error handling
-                        console.log('error', error);
-                }
-            }else if(decoded){
-                const redisKeySearch = 'jwtBL-' + deconded.exp;
-                
-                client.keys(redisKeySearch, (error, redisRes) => {
-                    console.log(redisRes);
-                });
 
-
-                if(decoded.exp - (Date.now()/1000) <= 300){
-                    const token = jwt.sign({username: decoded.username}, process.env.node_sess_secret, {algorithm: "HS256", expiresIn: process.env.node_sess_life });
-                    res.cookie('token', token, {maxAge: process.env.node_sess_life,  httpOnly: true, secure: true});
-	            
-                }
-
-                switch(path){
-                    case '/login':
-                        res.redirect('/test');    
-                        break;
-                    default:
-                        next();
-                }
+        const decoded = await jwtverify(token, process.env.node_sess_secret)
+        .then(decoded => {return decoded})
+        .catch(verifyError =>{
+            switch(verifyError['name']){
+                case 'TokenExpiredError':
+                    switch(path){
+                        case '/login':
+                            res.clearCookie('token');
+                            next();
+                            break;
+                        default:
+                            res.redirect('/login');
+                    }
+                    break;
+                default:
+                    // TODO error handling
+                    console.log('error', error);
             }
-        });   
+        });
+
+        const redisKeySearch = 'jwtbl-' + decoded.exp;
+        let jwtFound = false;
+
+        const redisKey = await clientKeys(redisKeySearch)
+            .then(redisKeyResults => {
+                return (redisKeyResults.length > 0 ? redisKeyResults : false);
+            })
+            .catch(redisKeyError => {
+                console.log(redisKeyError);
+                //TODO error handling
+            });
+        
+        if(redisKey !== false){
+            await clientlrange(redisKey[0], 0, -1)
+                .then(redisDateRes => {
+                    redisDateRes.forEach(redisToken => {
+                        console.log('redistoken',redisToken);
+                        console.log('token',token);
+                        if(redisToken === token){
+                            jwtFound = true;
+                        }
+                    });
+                })
+                .catch(redisDateerror => {
+                    console.log(redisDateerror);
+                    //TODO error handling
+            });
+        }
+
+        if(jwtFound === true && path !== '/logout'){
+            console.log('yup');
+            res.status(401).send('session has ended');
+        }else{
+            console.log('nope');
+            if(decoded.exp - (Date.now()/1000) <= 300){
+                const token = jwt.sign({username: decoded.username}, process.env.node_sess_secret, {algorithm: "HS256", expiresIn: process.env.node_sess_life });
+                res.cookie('token', token, {maxAge: process.env.node_sess_life,  httpOnly: true, secure: true});
+            
+            }
+
+            switch(path){
+                case '/login':
+                    res.redirect('/test');    
+                    break;
+                default:
+                    next();
+            }
+        } 
     }
 }
 
