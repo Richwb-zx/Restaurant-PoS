c4e9f79ccd670e9e377d6d2f88355694ac08e409
Richwb <57405742+Richwb@users.noreply.github.com>
2021-04-12 23:25:36 -0400

    Modified the transport class to input the log entry into the matching object parent (log type) in a JSON based log.

--- controller/winston.js
+++ controller/winston.js
@@ -1,9 +1,56 @@
 const winston = require('winston');
-const path = require('path');
 const util = require('util');
 const MESSAGE = Symbol.for('message');
+const Transport = require('winston-transport');
+const fs = require('fs');
 
-const logFile = (process.platform === 'win32' ? 'logs/' + process.env.nodeEnv + '/logs.json' :'/var/log/nodejs-winston/' + process.env.nodeEnv + '/logs.json');
+const logFile = (process.platform === 'win32' ? './logs/' + process.env.nodeEnv + '/logs.json' :'/var/log/nodejs-winston/' + process.env.nodeEnv + '/logs.json');
+
+class CustomTransport extends Transport {
+    constructor(opts){
+        super(opts);
+    }
+
+    readLog(){
+        let logJson = [];
+        if(fs.existsSync(logFile) === false){
+            this.createLog();
+        }
+
+        logJson = JSON.parse(fs.readFileSync(logFile, 'utf8'));
+
+        return logJson;
+    }
+
+    createLog(){
+        const logContent = '{"emerg":[],"alert":[],"crit":[],"error":[],"warning":[],"notice":[],"info":[],"debug":[]}';
+
+        fs.writeFileSync(logFile, logContent, 'utf8');
+    }
+
+    writeToLog(info){
+        const logJson = this.readLog();
+       
+        logJson[info.level].push(info);
+        const logStringify = JSON.stringify(logJson);
+
+        try{
+            fs.writeFileSync(logFile, logStringify, 'utf8');
+        }catch(error){
+            console.log('error');
+        }
+    }
+
+    log(info, callback){
+        setImmediate(() => {
+            this.emit('logged', info[MESSAGE]);
+        });
+        
+        this.writeToLog(JSON.parse(info[MESSAGE]));
+
+        callback();
+    }
+}
 
 const logTest = winston.createLogger({
     level: 'debug',
@@ -24,10 +71,11 @@ const logTest = winston.createLogger({
         })(),
     ),
     transports: [
-        new winston.transports.File({
+        new CustomTransport({
             filename: logFile,
-            level: 'info'
+            handleExceptions: true
         }),
+        new winston.transports.Console()
     ]
 });
 
