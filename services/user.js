65bb6880815ff2ab572c8f04972f5de9050153f5
Richwb <57405742+Richwb@users.noreply.github.com>
2021-04-22 20:58:37 -0400

    flipped logging messages and returned proper responses in jwt.sign

--- services/user.js
+++ services/user.js
@@ -28,9 +28,11 @@ const User = class User{
     setSession(logout = false){
         return jwt.sign({username: this.userName}, process.env.node_sess_secret, {algorithm: "HS256", expiresIn: process.env.node_sess_life }, (error, token) =>{      
             if(error !== undefined){
-                logger.crit({"message": {"code": escape(error)}, "user": "system", "namespace": 'users.setsession.jwt.sign'});
+                logger.info({"message": `User ${this.userName} has logged in`, "user": "system", "namespace": 'users.account.account.success'});
+                return token;
             }else{
-                logger.info({"message": this.userName +' has logged in', "user": "system", "namespace": 'users.account.account.success'});
+                logger.crit({"message": {"code": escape(error)}, "user": "system", "namespace": 'users.setsession.jwt.sign'});
+                return undefined;
             }
         });
     }
