83778af390e1451c28ab597b23c407cb6020b7c7
Richwb <57405742+Richwb@users.noreply.github.com>
2021-04-26 01:54:47 -0400

    setup relation mapping with routes_authorization

--- models/routes.js
+++ models/routes.js
@@ -6,6 +6,22 @@ class Routes extends Model{
     static get tableName(){
         return 'routes';
     }
+
+    static get relationMappings() {
+
+        const RoutesAuth = require('./routes_authorization.js');
+
+        return {
+            routesauth: {
+                relation: Model.HasManyRelation,
+                modelClass: RoutesAuth,
+                join: {
+                    from: 'routes.id',
+                    to: 'routes_authorization.route_id'
+                }
+            }
+        }
+    }
 }
 
 module.exports = Routes;
