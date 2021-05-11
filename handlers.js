b92d0607636ac0507ebf57b5709876b0b2e28349
Richwb <57405742+Richwb@users.noreply.github.com>
2021-05-10 20:17:24 -0400

    reworked authorization to check user group with request path permitted groups.

--- handlers.js
+++ handlers.js
@@ -3,21 +3,19 @@ const client = require('./lib/redis.js');
 const { promisify } = require('util');
 const getAsync = promisify(client.get).bind(client);
 const jwtverify = promisify(jwt.verify).bind(jwt);
-const Users = require('./models/users');
 
 const checkSession = async (req, res, next) => {
 
     const token = req.cookies.token;
     const pathName = req._parsedUrl.pathname;
     const httpMethod = req.method;
-    
-    // const routeAuthGroup = routeAuthData[0].group_name;
-    // console.log(routeAuthData);
+    const referrer = (req.get('Referrer') !== '' ? req.get('Referrer') : '/login');
     
     if(token === undefined){
-        const routeAuthData = await authorizationCheck(pathName,httpMethod, 1);
+        if(await authorizationCheck(pathName,httpMethod, 1) === false){
+            return res.redirect(301, referrer);
+        }
     }else{
-        console.log(req.headers);
         const decoded = await jwtverify(token, process.env.node_sess_secret)
         .then(decoded => {return decoded})
         .catch(verifyError =>{
@@ -25,68 +23,37 @@ const checkSession = async (req, res, next) => {
                 case 'TokenExpiredError':
                     switch(path){
                         case '/login':
-                            res.clearCookie('token');
-                            next();
+                            res.clearCookie('token').status('301').redirect(referrer);
+                            debugger;
                             break;
                         default:
-                            res.redirect('/login');
+                            return res.redirect(301, referrer);
                     }
                     break;
                 default:
-                    // TODO error handling
-                    console.log('error', error);
+                    logger.error({"message": {"code": escape(error)}, "user": decoded.group, "namespace": 'handlers.checkSession.jwtverify.error'});
             }
         });
 
-        const routeAuthData = await authorizationCheck(pathName,httpMethod, decoded.groupId);
-    }
-
-    // if(routeAuthGroup === 'Public' && token === undefined){
-
-    // }else if(routeAuthGroup === 'Public' && token !== undefined){
+        if(await authorizationCheck(pathName,httpMethod, decoded.group) === false){
+            return res.redirect(301, referrer);
+        }
         
-    // }else if(routeAuthGroup !== 'Public' && token !== undefined){
+        if(decoded.exp - (Date.now()/1000) <= 300){
+            const token = jwt.sign(
+                {user: decoded.user, group: decoded.group}, 
+                process.env.node_sess_secret, 
+                {algorithm: "HS256", expiresIn: process.env.node_sess_life 
+            });
 
-    // }
-
-    const path = req.path;
-    // TODO clean up after testing
-    if(path === '/test' || token === undefined && (path === '/login' || path === '/register' || path === '/loginauth')){
-        next();
-    }else if(token !== false && path === '/loginauth'){
-        res.redirect('/');
-    }else if(token === undefined && path !== '/login'){
-        res.redirect('/login');
-    }else{
-
-        
-
-        
-
-        if(jwtFound === true && path !== '/logout'){
-            console.log('yup');
-            res.status(401).send('session has ended');
-        }else{
-            console.log('nope');
-            if(decoded.exp - (Date.now()/1000) <= 300){
-                const token = jwt.sign({user: decoded.user, group: authGroup}, process.env.node_sess_secret, {algorithm: "HS256", expiresIn: process.env.node_sess_life });
-                res.cookie('token', token, {maxAge: process.env.node_sess_life,  httpOnly: true, secure: true});
-            
-            }
-
-            switch(path){
-                case '/login':
-                    res.redirect('/test');    
-                    break;
-                default:
-                    next();
-            }
+            res.cookie('token', token, {maxAge: process.env.node_sess_life,  httpOnly: true, secure: true});
         } 
     }
+
+    next();
 }
 
 async function authorizationCheck(route, method, groupId){
-    
     return await getAsync(`route-${route}-${method}-${groupId}`)
         .then(function(result){ 
             if(result !== null){
