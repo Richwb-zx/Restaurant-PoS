9d6864c351ac3525218b7128ea539f10606c18ef
Richwb <57405742+Richwb@users.noreply.github.com>
2021-05-17 00:02:05 -0400

    switched data-name to name property based on react docs

--- client/src/components/login/LoginForm.jsx
+++ client/src/components/login/LoginForm.jsx
@@ -12,15 +12,15 @@ class LoginForm extends React.Component{
   }
 
   updatefieldState(event){
-    const stateKey = event.target.dataset.name;
-    this.setState({[stateKey]: event.target.value});
+    const stateName = event.target.name;
+    this.setState({[stateName]: event.target.value});
   }
 
   render(){
     return (
       <form id="loginform" className="login-form">
-          <input type="text" placeholder="Username" value={this.state.username} onChange={this.updatefieldState} data-name="username" required />
-          <input type="password" placeholder="Password" value={this.state.password} onChange={this.updatefieldState} data-name="password" required></input>
+          <input type="text" name="username" value={this.state.username} onChange={this.updatefieldState} placeholder="Username" required />
+          <input type="password" name="password" value={this.state.password} onChange={this.updatefieldState} placeholder="Password"  required></input>
           <input type="submit" value="Login"/>
       </form>
     );
