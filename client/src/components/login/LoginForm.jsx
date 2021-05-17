6e3e0768b7049e37fff3e9c7223b296b066ab444
Richwb <57405742+Richwb@users.noreply.github.com>
2021-05-16 23:55:25 -0400

    added field states for later implimnetation for form submission.

--- client/src/components/login/LoginForm.jsx
+++ client/src/components/login/LoginForm.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './scss/loginForm.scss';
 class LoginForm extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+
+    this.updatefieldState = this.updatefieldState.bind(this);
+  }
+
+  updatefieldState(event){
+    const stateKey = event.target.dataset.name;
+    this.setState({[stateKey]: event.target.value});
+  }
+
   render(){
     return (
-      <form id="loginform" className="login-form" onSubmit="login">
-          <input type="text" placeholder="Username" required></input>
-          <input type="password" placeholder="Password" required></input>
+      <form id="loginform" className="login-form">
+          <input type="text" placeholder="Username" value={this.state.username} onChange={this.updatefieldState} data-name="username" required />
+          <input type="password" placeholder="Password" value={this.state.password} onChange={this.updatefieldState} data-name="password" required></input>
           <input type="submit" value="Login"/>
       </form>
     );
